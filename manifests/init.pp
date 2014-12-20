4059c58ef4aaa4f47b8c6fea6456bbfa5158f3b3
Robert Burns <burnsra@gmail.com>
2014-12-20 09:30:39 -0500

    Update to latest versions

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 # Examples
 #
 #   include firefox
-class firefox($locale = 'en-US', $version = '32.0'){
+class firefox($locale = 'en-US', $version = '34.0.5'){
   package { 'Firefox':
     source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/mac/${locale}/Firefox%20${version}.dmg",
     provider => 'appdmg'
--- spec/classes/firefox-beta_spec.rb
+++ spec/classes/firefox-beta_spec.rb
@@ -4,7 +4,7 @@ describe 'firefox::beta' do
   it do
     should contain_class('firefox::beta')
     should contain_package('Firefox-Beta').with({
-      :source   => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/33.0b4/mac/en-US/Firefox%2033.0b4.dmg',
+      :source   => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/35.0b5/mac/en-US/Firefox%2035.0b5.dmg',
       :provider => 'appdmg'
     })
   end
